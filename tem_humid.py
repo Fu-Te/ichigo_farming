e4ef6c89b1b6252e7c57eb7fc5434a9c67b16b52
F.T <ttyn4519@outlook.jp>
2021-09-08 22:32:17 +0900

    インデントエラーの修正

--- tem_humid.py
+++ tem_humid.py
@@ -19,13 +19,13 @@ try:
 	        print("Temperature: %-3.1f C" % result.temperature)
 	        print("Humidity: %-3.1f %%" % result.humidity)
 		#write temperature and humidity to csv file for gathering data.
-			with open('tem_humid.csv','a',newline='') as f:
-				writer=csv.writer(f,lineterminator='\n')
-				writer.writerow([datetime.datetime.now(),result.temperature,result.humidity])
+		with open('tem_humid.csv','a',newline='') as f:
+			writer=csv.writer(f,lineterminator='\n')
+			writer.writerow([datetime.datetime.now(),result.temperature,result.humidity])
 
 
 	    time.sleep(6)
-		
+
 
 except KeyboardInterrupt:
     print("Cleanup")
