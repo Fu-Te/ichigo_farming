ebd84862f9b7799749100595ef2970668e26fdaa
F.T <ttyn4519@outlook.jp>
2021-09-10 17:42:19 +0900

    Update tem_humid.py

--- tem_humid.py
+++ tem_humid.py
@@ -12,22 +12,22 @@ instance = dht11.DHT11(pin=14)
 
 try:
 	while True:
-	    result = instance.read()
-	    if result.is_valid():
-	        print("Last valid input: " + str(datetime.datetime.now()))
+		result = instance.read()
+		if result.is_valid():
+			print("Last valid input: " + str(datetime.datetime.now()))
 
-	        print("Temperature: %-3.1f C" % result.temperature)
-	        print("Humidity: %-3.1f %%" % result.humidity)
+			print("Temperature: %-3.1f C" % result.temperature)
+			print("Humidity: %-3.1f %%" % result.humidity)
 		#write temperature and humidity to csv file for gathering data.
 		with open('tem_humid.csv','a',newline='') as f:
 			writer=csv.writer(f,lineterminator='\n')
 			writer.writerow([datetime.datetime.now(),result.temperature,result.humidity])
 
 
-	    time.sleep(6)
+		time.sleep(6)
 
 
 except KeyboardInterrupt:
-    print("Cleanup")
-    GPIO.cleanup()
+	print("Cleanup")
+	GPIO.cleanup()
 
