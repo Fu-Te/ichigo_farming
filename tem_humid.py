eb49987150b6ae4450648c1333efbf59f461e2a9
F.T <ttyn4519@outlook.jp>
2021-09-10 17:28:10 +0900

    Update tem_humid.py

--- tem_humid.py
+++ tem_humid.py
@@ -19,7 +19,7 @@ try:
 	        print("Temperature: %-3.1f C" % result.temperature)
 	        print("Humidity: %-3.1f %%" % result.humidity)
 		#write temperature and humidity to csv file for gathering data.
-		with open('tem_humid.csv','a',newline='') as f: 
+		with open('tem_humid.csv','a',newline='') as f:
 			writer=csv.writer(f,lineterminator='\n')
 			writer.writerow([datetime.datetime.now(),result.temperature,result.humidity])
 
