c7a40b3e14ec438885d08c90c7f76dd746b6b7e5
F.T <ttyn4519@outlook.jp>
2021-09-20 17:42:52 +0900

    Update tem_humid.py

--- tem_humid.py
+++ tem_humid.py
@@ -18,20 +18,29 @@ ambi=ambient.Ambient(41563,'ba0c12f7851e4dad')
 def take_temp_humid():
 	result = instance.read()
 	if result.is_valid():
-		print('日時:' + str(datetime.datetime.now()))
-		print('気温: %-3.1f C' % result.temperature)
-		print('湿度: %-3.1f %%' % result.humidity)
+		if result.humidity != 0 or result.temperature != 0:
+
+			print('日時:' + str(datetime.datetime.now()))
+			print('気温: %-3.1f C' % result.temperature)
+			print('湿度: %-3.1f %%' % result.humidity)
+		else:
+			print('湿度温度取得失敗')
 
 def send_ambi():
 	result = instance.read()
-	r=ambi.send({'d1':result.temperature, 'd2':result.humidity})
-	print('ambiへ送信')
+	
+	if result.humidity != 0 or result.temperature != 0:
+		r=ambi.send({'d1':result.temperature, 'd2':result.humidity})
+		print('ambiへ送信')
+	else:
+		print('温度湿度取得失敗したため送信しません．')
 
 def store_data():
 	result = instance.read()
 	with open('tem_humid.csv','a',newline='') as f:
-		writer=csv.writer(f,lineterminator='\n')
-		writer.writerow([datetime.datetime.now(),result.temperature,result.humidity])
+		if result.humidity != 0 or result.temperature != 0:
+			writer=csv.writer(f,lineterminator='\n')
+			writer.writerow([datetime.datetime.now(),result.temperature,result.humidity])
 
 try:
 	while True:
