f424970f5fd8dcf3d3fee9e9d35f29903a63047e
F.T <ttyn4519@outlook.jp>
2021-09-21 16:24:31 +0900

    change

--- web_test/app.py
+++ web_test/app.py
@@ -1,11 +1,15 @@
 from flask import Flask,render_template
+from tem_humid import take_temp_humid
+
 
 app = Flask(__name__)
 @app.route('/')
 #def hello_world():
 #	return 'hello_world'
 def main():
+
 	return render_template('main.html')
+
 
 
 app.run(port=8000,debug=True)
