b0c132e0b6678546c8ef474cd6bf6429a4365bd1
F.T <ttyn4519@outlook.jp>
2021-10-18 00:01:08 +0900

    add nrf

--- peripheral/temperature/main.c
+++ peripheral/temperature/main.c
@@ -1,107 +1,121 @@
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
-* @defgroup temperature_example_main main.c
-* @{
-* @ingroup temperature_example
-* @brief Temperature Example Application main file.
-* @details
-* This file contains the source code for a sample application using the temperature sensor.
-* This contains workaround for PAN_028 rev2.0A anomalies 28, 29,30 and 31. PAN 43 is not covered.
-*  - PAN_028 rev2.0A anomaly 28 - TEMP: Negative measured values are not represented correctly
-*  - PAN_028 rev2.0A anomaly 29 - TEMP: Stop task clears the TEMP register.
-*  - PAN_028 rev2.0A anomaly 30 - TEMP: Temp module analog front end does not power down when DATARDY event occurs.
-*  - PAN_028 rev2.0A anomaly 31 - TEMP: Temperature offset value has to be manually loaded to the TEMP module
-*  - PAN_028 rev2.0A anomaly 43 - TEMP: Using PPI between DATARDY event and START task is not functional.
-*
-*/
+
 
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "nrf.h"
 #include "nrf_delay.h"
-#include "nrf_temp.h"
 #include "app_error.h"
 #include "bsp.h"
 
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
-/** @brief Function for main application entry.
- */
+
+
+#define DHT11_PIN 7
+
+int DHT11ReadByte(void)
+{
+  int ByteData=0,i;
+  unsigned long int counter=0;
+  for(i=0;i<8;i++)
+  {
+    ByteData = ByteData * 2;
+    counter = 0;
+    while(0==(nrf_gpio_pin_read(DHT11_PIN))) // may be 50us
+    {
+      counter++;
+      nrf_delay_us(1);
+      if ( 200 < counter  ){
+        NRF_LOG_INFO("error1\n");
+        return -1;
+      }
+    }
+    counter = 0;
+    while(1==(nrf_gpio_pin_read(DHT11_PIN)))
+    {
+      counter++;
+      nrf_delay_us(1);
+      if ( 200 < counter  ){
+        NRF_LOG_INFO("error2\n");
+        return -1;
+      }
+    }
+    if(counter<30)  // 40... error
+    {
+            // 26-28us : Bit data "0"
+    } else {
+      ByteData = ByteData +1; // 70us : Bit data "1"
+    }           
+  }
+  return ByteData;
+}
+
+void DHT11ReadData()
+{   
+  int counter,err,humiInt,humiDec,tempInt,tempDec,checkSum,decimal_value1,decimal_value2,decimal_value3;
+  nrf_gpio_pin_set(DHT11_PIN);
+  nrf_delay_ms(250);
+  nrf_gpio_cfg_output(DHT11_PIN);
+  nrf_gpio_pin_clear(DHT11_PIN);   // Send Start Signal
+  nrf_delay_ms(20);   
+  nrf_gpio_pin_set(DHT11_PIN);
+  nrf_delay_us(40);                // Send Start Signal end
+  nrf_gpio_cfg_input(DHT11_PIN, NRF_GPIO_PIN_PULLUP);
+  nrf_delay_us(1);
+  counter=0;
+  while(nrf_gpio_pin_read(DHT11_PIN)==0) // Receive LOW answer ( 83us)
+  {
+    nrf_delay_us(1);
+    counter++;
+    if ( 200 < counter ){
+        NRF_LOG_INFO("got wrong start1\n");
+        return;
+    }
+  }
+  counter=0;
+  while(nrf_gpio_pin_read(DHT11_PIN)==1) // Recieve High answer ( 87us)
+  {
+    nrf_delay_us(1);
+    counter++;
+    if ( 200 < counter ){
+      NRF_LOG_INFO("got wrong start2\n");
+      return;
+    }
+  }
+  humiInt = DHT11ReadByte();
+  humiDec = DHT11ReadByte();
+  tempInt = DHT11ReadByte();
+  tempDec = DHT11ReadByte();
+  checkSum = DHT11ReadByte();
+
+  if (((humiInt + humiDec + tempInt + tempDec) & 0xff) != checkSum) 
+  {
+     NRF_LOG_INFO("got wrong values\n");
+     return -1;
+  } else {
+    if ( 127 < tempDec ){
+      tempInt=0-tempInt;
+      tempDec=tempDec-128;
+    }
+    NRF_LOG_INFO("TEMP: %d.%d  HUMI: %d.%d",tempInt,tempDec,humiInt,humiDec);
+  }
+}
+
+
 int main(void)
 {
-    // This function contains workaround for PAN_028 rev2.0A anomalies 28, 29,30 and 31.
-    int32_t volatile temp;
-
-    nrf_temp_init();
-
     APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
     NRF_LOG_DEFAULT_BACKENDS_INIT();
-    NRF_LOG_INFO("Temperature example started.");
-
+    NRF_LOG_INFO("Temp/Humi sensor DHT11 example started.");
+    nrf_gpio_cfg_input(DHT11_PIN, NRF_GPIO_PIN_PULLUP);
+    SEGGER_RTT_WriteString(0, "DHT11 init complete\n");
     while (true)
     {
-        NRF_TEMP->TASKS_START = 1; /** Start the temperature measurement. */
-
-        /* Busy wait while temperature measurement is not finished, you can skip waiting if you enable interrupt for DATARDY event and read the result in the interrupt. */
-        /*lint -e{845} // A zero has been given as right argument to operator '|'" */
-        while (NRF_TEMP->EVENTS_DATARDY == 0)
-        {
-            // Do nothing.
-        }
-        NRF_TEMP->EVENTS_DATARDY = 0;
-
-        /**@note Workaround for PAN_028 rev2.0A anomaly 29 - TEMP: Stop task clears the TEMP register. */
-        temp = (nrf_temp_read() / 4);
-
-        /**@note Workaround for PAN_028 rev2.0A anomaly 30 - TEMP: Temp module analog front end does not power down when DATARDY event occurs. */
-        NRF_TEMP->TASKS_STOP = 1; /** Stop the temperature measurement. */
-
-        NRF_LOG_INFO("Actual temperature: %d", (int)temp);
-        nrf_delay_ms(500);
-
+        DHT11ReadData();
+        nrf_delay_ms(3000);
         NRF_LOG_FLUSH();
     }
 }
 
-
-/** @} */
---
+++ peripheral/temperature/pca10056/blank/ses/Output/Release/Obj/temperature_pca10056/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/temperature_pca10056/thumb_crt0.o: \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42b/source/thumb_crt0.s
