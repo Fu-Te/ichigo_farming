d4b3cfebcc9a74f64d9f84ef605e60b45169f48f
F.T <ttyn4519@outlook.jp>
2021-12-02 20:30:37 +0900

    Update discover.py

--- take_sensor_data/discover.py
+++ take_sensor_data/discover.py
@@ -4,8 +4,8 @@ import asyncio
 from bleak import BleakScanner
 
 async def main():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(d)
+	devices = await BleakScanner.discover()
+	for d in devices:
+		print(d)
 
 asyncio.run(main())
