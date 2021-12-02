139639d7dae379f6722ab6b86d9dcb8ebe2adc82
F.T <ttyn4519@outlook.jp>
2021-12-02 19:25:30 +0900

    Update discover.py

--- take_sensor_data/discover.py
+++ take_sensor_data/discover.py
@@ -1,15 +1,17 @@
-#addressを探すためのコード
+# addressを探すためのコード
 import asyncio
 
 from bleak import discover
 
-def scan():
-	async def run():
-		devices = await discover()
-		for d in devices:
-			print(d)
 
-	loop = asyncio.get_event_loop()
-	return loop.run_until_complete(run())
+def scan():
+    async def run():
+        devices = await discover()
+        for d in devices:
+            print(d)
+
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(run())
+
 
 scan()
