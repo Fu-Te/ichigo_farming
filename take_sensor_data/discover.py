e105289e13a9997e4f9744d34d580f5ec53118de
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-05-16 03:03:37 +0900

    Update discover.py

--- take_sensor_data/discover.py
+++ take_sensor_data/discover.py
@@ -1,11 +1,27 @@
 # addressを探すためのコード
 import asyncio
+import hashlib
 
 from bleak import BleakScanner
 
+near_device=[]
+hash_device=[]
+
 async def scan():
-	devices = await BleakScanner.discover()
-	for d in devices:
-		print(d)
+    devices = await BleakScanner.discover()
+    for d in devices:
+        d,c=str(d).split(':')
+        near_device.append(d)
+        
+        
+        d=hashlib.md5(d.encode()).hexdigest()
+        hash_device.append(d)
+        
 
 asyncio.run(scan())
+
+
+
+
+print(near_device)
+print(hash_device)
