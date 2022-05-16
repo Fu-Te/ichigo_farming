725e57e350618d3c181ce23a6ad25a060958f051
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-05-16 09:18:51 +0900

    Update discover.py

--- take_sensor_data/discover.py
+++ take_sensor_data/discover.py
@@ -4,12 +4,14 @@ import hashlib
 
 from bleak import BleakScanner
 
+raw_data=[]
 near_device=[]
 hash_device=[]
 
 async def scan():
     devices = await BleakScanner.discover()
     for d in devices:
+        raw_data.append(d)
         d,c=str(d).split(':')
         near_device.append(d)
         
@@ -22,6 +24,8 @@ asyncio.run(scan())
 
 
 
-
+print(raw_data)
+print('___________________________________________')
 print(near_device)
+print('___________________________________________')
 print(hash_device)
