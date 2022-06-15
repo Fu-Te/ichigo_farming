77e09194e6d46254817e786efd0073732f66c300
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-06-16 06:41:08 +0900

    raspberrypiに対応

---
+++ bluetooth_setting/README.md
@@ -0,0 +1,6 @@
+# how to use
+
+```
+$ sudo apt-get install libatlas-base-dev
+$ sudo apt-get install libjasper-dev
+```
--- bluetooth_setting/discover.py
+++ bluetooth_setting/discover.py
@@ -1,31 +1,37 @@
 # addressを探すためのコード
 import asyncio
 import hashlib
+import pandas as pd
 
 from bleak import BleakScanner
 
-raw_data=[]
-near_device=[]
-hash_device=[]
+raw_data = []
+near_device = []
+hash_device = []
 
 async def scan():
     devices = await BleakScanner.discover()
     for d in devices:
         raw_data.append(d)
-        d,c=str(d).split(':')
+
+        d,c = str(d).split(': ')
         near_device.append(d)
         
         
-        d=hashlib.md5(d.encode()).hexdigest()
+        d = hashlib.md5(d.encode()).hexdigest()
         hash_device.append(d)
         
 
 asyncio.run(scan())
 
+df = pd.DataFrame(list(zip(near_device, hash_device)), columns = ['near_device','hash_device'])
+
+df['device1_flag'] = ''
+df['device2_flag'] = ''
+df['device3_flag'] = ''
+df['device4_flag'] = ''
 
 
-print(raw_data)
-print('___________________________________________')
-print(near_device)
-print('___________________________________________')
-print(hash_device)
+
+print(df)
+df.to_csv('result/test.csv',index=False , encoding = 'utf_8_sig')
