ec622da9918fe97726570ae287cae6542ea17e9d
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-07-12 07:21:32 +0900

    add

--- rasp/bluetooth_setting/discover.py
+++ rasp/bluetooth_setting/discover.py
@@ -1,37 +1,25 @@
 # addressを探すためのコード
 import asyncio
-import hashlib
 import pandas as pd
 
 from bleak import BleakScanner
 
-raw_data = []
-near_device = []
-hash_device = []
-
 async def scan():
+    raw_data = []
+    near_device = []
+    device_name = []
+    
+    
     devices = await BleakScanner.discover()
     for d in devices:
         raw_data.append(d)
 
         d,c = str(d).split(': ')
         near_device.append(d)
+        device_name.append(c)
         
-        
-        d = hashlib.md5(d.encode()).hexdigest()
-        hash_device.append(d)
-        
-
+        df = pd.DataFrame(list(zip(near_device, device_name)), columns = ['near_device', 'device_name'])
+        print(df)
+    return df
 asyncio.run(scan())
 
-df = pd.DataFrame(list(zip(near_device, hash_device)), columns = ['near_device','hash_device'])
-
-df['device1_flag'] = ''
-df['device2_flag'] = ''
-df['device3_flag'] = ''
-df['device4_flag'] = ''
-
-
-
-print(df)
-df.to_csv('result/test.csv',index=False , encoding = 'utf_8_sig')
