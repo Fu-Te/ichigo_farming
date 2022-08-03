c05acff8f2a82875e347b527bd0c381a1973ce3f
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-08-04 02:02:13 +0900

    making as def

--- ble/l2cap_client.py
+++ ble/l2cap_client.py
@@ -1,30 +1,38 @@
 import sys
 import bluetooth
 
-if sys.version < '3':
-    input = raw_input
+def l2cap_client(bt_addr,data):
+    if sys.version < '3':
+        input = raw_input
+        
+    sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+    if len(sys.argv) < 2:
+        print('usage: l2capclient.py <addr>')
+        sys.exit(2)
+        
+    port = 0x1001
+
+    print(f'trying to connect to {bt_addr} on PSM 0x{port}')
+
+    sock.connect((bt_addr,port))
+
+    print('connected. type stuff')
+
+    while True:
+        if(len(data) == 0):
+            break
+        sock.send(data) 
+        data = sock.recv(1024)
+        print(f'Data received:{str(data)}')
+        
+    sock.close()
     
-sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
-if len(sys.argv) < 2:
-    print('usage: l2capclient.py <addr>')
-    sys.exit(2)
-    
-bt_addr = sys.argv[1]
-port = 0x1001
 
-print(f'trying to connect to {bt_addr} on PSM 0x{port}')
+bt_addr_list = ['B8:27:EB:7D:E6:F6','E4:5F:01:38:C5:37']
+bt_addr = 'E4:5F:01:38:C5:37'
+data = 'aaa'
 
-sock.connect((bt_addr,port))
 
-print('connected. type stuff')
-
-while True:
-    data = input()
-    if(len(data) == 0):
-        break
-    sock.send(data) 
-    data = sock.recv(1024)
-    print(f'Data received:{str(data)}')
-    
-sock.close()
+l2cap_client(bt_addr,data)
--- ble/l2cap_server.py
+++ ble/l2cap_server.py
@@ -1,33 +1,41 @@
 from http import server
 import bluetooth
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-server_sock.bind(('',0x1001))
-server_sock.listen(1)
 
-while True:
-    print('送信されてくるデータを待ってます')
-    client_sock, address = server_sock.accept()
-    print(f'データを受け取りました{str(address)}')
-    
-    print('データを受信中')
-    
-    total = 0
+def l2cap_server():
+    server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+    server_sock.bind(('',0x1001))
+    server_sock.listen(1)
+
     while True:
-        try:
-            data = client_sock.recv(1024)
-        except bluetooth.BluetoothError as e:
-            break
+        print('送信されてくるデータを待ってます')
+        client_sock, address = server_sock.accept()
+        print(f'データを受け取りました{str(address)}')
         
-        if len(data) == 0:
-            break
+        print('データを受信中')
         
-        total += len(data)
-        print(f'total byte read:{total}')
-        print(f'data is: {data}')
+        total = 0
+        total_data = ''
+        while True:
+            try:
+                data = client_sock.recv(1024)
+            except bluetooth.BluetoothError as e:
+                break
+            
+            if len(data) == 0:
+                break
+            
+            total += len(data)
+            
+            print(f'total byte read:{total}')
+            print(f'data is: {data}')
+            
+        client_sock.close()
         
-    client_sock.close()
+        print('connection closed')
+        
+    server_sock.close()
     
-    print('connection closed')
-    
-server_sock.close()
+    return data
+
+data = l2cap_server()
