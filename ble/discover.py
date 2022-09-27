fff91a539c4a06687f1f006978e64c194c2de389
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2022-09-27 23:26:30 +0900

    apply pep8

--- ble/discover.py
+++ ble/discover.py
@@ -4,35 +4,35 @@ import pandas as pd
 
 from bleak import BleakScanner
 
+
 async def scan():
     """
     周囲のデバイスをスキャンし，端末名とアドレスをデータフレーム形式で出力する
-    
+
     Parameters
     ----------
-    
-    
+
+
     Return
     ----------
     df : DataFrame
         周囲のBLEでスキャンできるすべての端末情報
-    
+
     Notes
-    ---------- 
+    ----------
     """
     raw_data = []
     near_device = []
     device_name = []
-    
-    
+
     devices = await BleakScanner.discover()
     for d in devices:
         raw_data.append(d)
 
-        d,c = str(d).split(': ')
+        d, c = str(d).split(': ')
         near_device.append(d)
         device_name.append(c)
-        
-        df = pd.DataFrame(list(zip(near_device, device_name)), columns = ['near_device', 'device_name'])
-    return df
 
+        df = pd.DataFrame(list(zip(near_device, device_name)),
+                          columns=['near_device', 'device_name'])
+    return df
--- ble/l2cap_server.py
+++ ble/l2cap_server.py
@@ -6,28 +6,28 @@ def l2cap_server():
     """
     BLE経由で情報を受け取る.
     l2cap_client.pyで送信されてくるデータを受信することができる.
-    
+
     Parameters
     ----------
-    
+
     Return
     ----------
-    
+
     Notes
-    
-     
+
+
     """
     server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-    server_sock.bind(('',0x1001))
+    server_sock.bind(('', 0x1001))
     server_sock.listen(1)
 
     while True:
         print('送信されてくるデータを待ってます')
         client_sock, address = server_sock.accept()
         print(f'データを受け取りました{str(address)}')
-        
+
         print('データを受信中')
-        
+
         total = 0
         total_data = ''
         while True:
@@ -35,20 +35,20 @@ def l2cap_server():
                 data = client_sock.recv(1024)
             except bluetooth.BluetoothError as e:
                 break
-            
+
             if len(data) == 0:
                 break
-            
+
             total += len(data)
-            
+
             print(f'total byte read:{total}')
             print(f'data is: {data}')
-            
+
         client_sock.close()
-        
+
         print('connection closed')
         return data
-        
+
     server_sock.close()
 
 
@@ -62,5 +62,6 @@ def l2cap_server_main(receive_data_list):
             break
 
     return receive_data_list
+
 
 data = l2cap_server()
