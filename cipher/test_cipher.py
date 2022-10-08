b2c8c6b470301c5175d280eadc17db79c103d623
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2022-10-08 22:05:31 +0900

    pytestが通るように修正

--- cipher/test_cipher.py
+++ cipher/test_cipher.py
@@ -4,6 +4,7 @@ from cipher import make_signature
 from cipher import judge_signature
 import pandas as pd
 
+
 def test_make_key():
     secret_k, public_k = make_key()
     print(f'秘密鍵{secret_k}:公開鍵{public_k}')
@@ -15,7 +16,19 @@ def test_make_signature():
     signature = ''
     secret_k, public_k = make_key()
     df = pd.read_csv('../test_data_folder/cipher/sample1.csv')
+    
+    
+    #署名はbytesにしか対応してない
     signature = make_signature(secret_k, df)
     print(f'署名{signature}')
     assert signature != ''
-test_make_signature()
+    
+    
+def test_judge_signature():
+    secret_k, public_k = make_key()
+    df = pd.read_csv('../test_data_folder/cipher/sample1.csv')
+    signature = make_signature(secret_k, df)
+    
+    result = judge_signature(signature, df, public_k)
+    
+    assert result == True
