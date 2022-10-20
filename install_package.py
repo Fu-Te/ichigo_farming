814c218bfa5220cf5d68ad06aaa52d8c95af7693
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2022-10-21 05:12:52 +0900

    Update install_package.py

--- install_package.py
+++ install_package.py
@@ -1,7 +1,10 @@
 import subprocess
 
-#pip install -r requirements.txt 必要なパッケージの一括インストール
+#必要なパッケージの一括インストール
 def install_package():
+    subprocess.run(['sudo','apt-get','install','libatlas-base-dev'])
+    subprocess.run(['sudo','apt-get','install','libjasper-dev'])
+    subprocess.run(['sudo','apt-get','install','bluetooth','libbluetooth-dev'])
     subprocess.run(['pip','install','-r','requirements.txt'])
     
 install_package()
