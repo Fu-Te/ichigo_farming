2d3fa73287f0c167c7a6a75f5caae6a084d4ca8f
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2022-12-19 11:12:19 +0900

    Create pandas_d_encode.py

---
+++ ble/pandas_d_encode.py
@@ -0,0 +1,29 @@
+
+def pandas_encode(df):
+    """pandas_encode
+    データフレームをバイト形式に変換する．
+    
+    Parameters
+    ----------
+    df:DataFrame 変換したいデータフレーム
+    
+    Return
+    ----------
+    バイト形式のDataFrame
+    
+    """
+    
+def pandas_decode(df):
+    """pandas_decode
+    
+    Parameters
+    ----------
+    df: バイト形式のデータフレーム
+    
+    Return
+    ----------
+    バイト形式でないのデータフレーム
+
+    Args:
+        df (_type_): _description_
+    """
