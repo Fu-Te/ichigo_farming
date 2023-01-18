c75242380ff9f69a64269022a58752160ccee75a
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-01-18 12:04:56 +0900

    add docstring

--- install_package.py
+++ install_package.py
@@ -2,6 +2,16 @@ import subprocess
 
 #必要なパッケージの一括インストール
 def install_package():
+    '''
+    必要なパッケージの一括インストール
+    
+    Parameters
+    ----------
+    
+    Return
+    ----------
+    
+    '''
     subprocess.run(['sudo','apt-get','install','libatlas-base-dev'])
     subprocess.run(['sudo','apt-get','install','libjasper-dev'])
     subprocess.run(['sudo','apt-get','install','bluetooth','libbluetooth-dev'])
